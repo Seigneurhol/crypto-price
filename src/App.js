ca9d115004167260b589762feb9b5ae8ee0c2b1c
Seigneurhol <seigneurholo@hotmail.fr>
2019-05-16 20:02:13 +0200

    Refactor some App html code into Crypto childen components

--- src/App.js
+++ src/App.js
@@ -1,89 +1,71 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Crypto } from "./Crypto/Crypto";
+import Crypto from "./Crypto/Crypto";
 import Navbar from "./Navbar/Navbar";
 
-function App() {
-  return (
-    <div className="app">
-      <Navbar />
-      <div className="container-fluid">
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Nom</th>
-              <th scope="col">Prix</th>
-              <th scope="col">Change</th>
-              <th scope="col">Volume</th>
-              <th scope="col">Capitalisation du marché</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>
-                <div className="row align-items-center">
-                  <div className="col-5">
-                    <img
-                      src="images/Bitcoin.png"
-                      className="card-img-top"
-                      alt="Bitcoin"
-                    />
-                  </div>
-                  <div className="col-5">Bitcoin</div>
-                </div>
-              </td>
-              <td>8000$</td>
-              <td>+5%</td>
-              <td>29 679 894 734$</td>
-              <td>125 983 025 437$</td>
-            </tr>
-            <tr>
-              <th scope="row align-items-center">1</th>
-              <td>
-                <div className="row align-items-center">
-                  <div className="col-5">
-                    <img
-                      src="images/Ethereum.png"
-                      className="card-img-top"
-                      alt="Ethereum"
-                    />
-                  </div>
-                  <div className="col-5">Ethereum</div>
-                </div>
-              </td>
-              <td>8000$</td>
-              <td>+5%</td>
-              <td>29 679 894 734$</td>
-              <td>125 983 025 437$</td>
-            </tr>
-            <tr>
-              <th scope="row">1</th>
-              <td>
-                <div className="row align-items-center">
-                  <div className="col-5">
-                    <img
-                      src="images/Monero.png"
-                      className="card-img-top"
-                      alt="Monero"
-                    />
-                  </div>
-                  <div className="col-5">Monero</div>
-                </div>
-              </td>
-              <td>8000$</td>
-              <td>+5%</td>
-              <td>29 679 894 734$</td>
-              <td>125 983 025 437$</td>
-            </tr>
-          </tbody>
-        </table>
-        <Crypto />
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      crypto: [
+        {
+          index: 1,
+          name: "Bitcoin",
+          imageURL: "images/Bitcoin.png",
+          price: "$8000",
+          change: "+5%",
+          volume: "$29 679 894 734",
+          marketcap: "$125 983 025 437"
+        },
+        {
+          index: 2,
+          name: "Ethereum",
+          imageURL: "images/Ethereum.png",
+          price: "$8000",
+          change: "+5%",
+          volume: "$29 679 894 734",
+          marketcap: "$125 983 025 437"
+        },
+        {
+          index: 3,
+          name: "Monero",
+          imageURL: "images/Monero.png",
+          price: "$8000",
+          change: "+5%",
+          volume: "$29 679 894 734",
+          marketcap: "$125 983 025 437"
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="app">
+        <Navbar />
+        <div className="container-fluid">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Prix</th>
+                <th scope="col">Change</th>
+                <th scope="col">Volume</th>
+                <th scope="col">Capitalisation du marché</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.crypto.map(crypto => (
+                <Crypto info={crypto} key={crypto.index} />
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Crypto/Crypto.js
+++ src/Crypto/Crypto.js
@@ -2,75 +2,28 @@ import React, { Component } from "react";
 import "./Crypto.css";
 
 // Déplacer chacune des cartes dans une classe crypto et le deck dans App
-export class Crypto extends Component {
-  render() {
-    return (
-        <div></div>
-      /*<div className="crypto">
-        <div className="card-deck">
-          <div className="card">
-            <div class="align-self-center">
-              <img
-                src="images/Bitcoin.png"
-                className="card-img-top"
-                alt="Bitcoin"
-              />
-            </div>
-            <div className="card-body">
-              <h5 className="card-title">Bitcoin</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p className="card-text">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </p>
-            </div>
+function Crypto(props) {
+  return (
+    <tr>
+      <th scope="row">{props.info.index}</th>
+      <td>
+        <div className="row align-items-center">
+          <div className="col-5">
+            <img
+              src={props.info.imageURL}
+              className="card-img-top"
+              alt={props.info.name}
+            />
           </div>
-          <div className="card">
-            <div class="align-self-center">
-              <img
-                src="images/Ethereum.png"
-                className="card-img-top"
-                alt="Ethereum"
-              />
-            </div>
-            <div className="card-body">
-              <h5 className="card-title">Ethereum</h5>
-              <p className="card-text">
-                This card has supporting text below as a natural lead-in to
-                additional content.
-              </p>
-              <p className="card-text">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </p>
-            </div>
-          </div>
-          <div className="card">
-            <div class="align-self-center">
-              <img
-                src="images/Monero.png"
-                className="card-img-top"
-                alt="Monero"
-              />
-            </div>
-            <div className="card-body">
-              <h5 className="card-title">Monero</h5>
-              <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
-              </p>
-              <p className="card-text">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </p>
-            </div>
-          </div>
+          <div className="col-5">{props.info.name}</div>
         </div>
-      </div>*/
-    );
-  }
+      </td>
+      <td>{props.info.price}</td>
+      <td>{props.info.change}</td>
+      <td>{props.info.volume}</td>
+      <td>{props.info.marketcap}</td>
+    </tr>
+  );
 }
 
 export default Crypto;
